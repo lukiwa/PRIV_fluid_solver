7100664a32db20b31a68161a0b0d9ab4e116e19a
Lukasz Iwan <lukasz.iwan.99@gmail.com>
2020-06-04 12:58:33 +0200

    edited gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include "Array2D.h"
+#include "SFML/Graphics.hpp"
+
+//if using Windows Subsystem for Linux - this allows viewing sfml
+#define WSL_ALLOW_DISPLAY setenv("DISPLAY", "127.0.0.1:0", true)
 
 int main() {
-    Array2D<int> arr(5);
+    WSL_ALLOW_DISPLAY;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
+    // run the program as long as the window is open
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 
 
     return 0;

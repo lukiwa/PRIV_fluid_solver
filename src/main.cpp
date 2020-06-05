f5e1b846375cef0a4d281a4a5ce7d2fc028f4666
Lukasz <lukasz.iwan.99@gmail.com>
2020-06-05 23:30:43 +0200

    Changes in fluid settings

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,10 @@ int main() {
     DisplayableFluidBuilder builder;
     sf::RenderWindow window;
     auto fluid = builder.Size(200).
-            TimeStep(0.1).
+            TimeStep(0.25).
             Diffusion(0.0000001).
             Viscosity(0.0000001).
-            Fade(0.5).
+            Fade(0.2).
             Build(window);
 
 
@@ -31,16 +31,17 @@ int main() {
             }
         }
 
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                fluid.AddDensity(window.getSize().x / 2, window.getSize().y / 2, std::rand() % 120);
-            }
-        }
+        //generate random velocities
         float rand1 = -1.f + static_cast <float> (std::rand()) / (static_cast <float> (RAND_MAX / (1.f + 1.f)));
         float rand2 = -1.f + static_cast <float> (std::rand()) / (static_cast <float> (RAND_MAX / (1.f + 1.f)));
 
+        //add some density at the middle of the screen
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                fluid.AddDensity(window.getSize().x / 2, window.getSize().y / 2, std::rand() % 100);
+            }
+        }
         fluid.AddVelocity(window.getSize().x / 2, window.getSize().y / 2, rand1, rand2);
-
         window.clear(sf::Color::White);
         fluid.Render();
         window.display();
